b3154f811b3e64ab4ef3899808c605c71d6bb64e
Daniel Cox <156420435+dcox761@users.noreply.github.com>
2024-03-11 04:50:17 +0000

    Input field for API Address using pinia and local storage

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,40 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+// https://runthatline.com/how-to-use-local-storage-pinia/
+import { createPinia, defineStore } from 'pinia'
+
+const STORE_NAME = 'main'
+
+const getDefaultSettings = () => ({
+  apiAddress: null
+})
+
+const getSettings = () => {
+  const settings = localStorage.getItem(STORE_NAME)
+
+  return settings ? JSON.parse(settings) : getDefaultSettings()
+}
+
+export const useStore = defineStore(STORE_NAME, {
+  state: () => ({
+    settings: getSettings(),
+  }),
+  actions: {
+    updateSettings(partialSettings: any) {
+      this.settings = {
+        ...this.settings,
+        ...partialSettings,
+      }
+      localStorage.setItem(STORE_NAME, JSON.stringify(this.settings))
+    },
+  },
+})
+
+const pinia = createPinia()
+
 const app = createApp(App)
+  .use(pinia)
   .use(IonicVue)
   .use(router);
   
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -11,13 +11,36 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+      <ion-list>
+        <ion-item>
+          <ion-input label="API Address" v-model="settings.apiAddress"/>
+        </ion-item>
+        <ion-item>
+          <ion-button @click="handleConnectClick">Connect</ion-button>
+        </ion-item>
+      </ion-list>
+      <!-- <ExploreContainer name="Tab 1 page ..." /> -->
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { storeToRefs } from 'pinia'
+import { useStore } from '../main'
+  
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonList, IonItem, IonButton } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+
+const store = useStore()
+const { settings } = storeToRefs(store)
+
+const handleConnectClick = () => {
+    console.log("CLICK: " + settings.value.apiAddress)
+    store.updateSettings({
+      apiAddress: settings.value.apiAddress
+    })
+
+    // TODO: check connection, switch to status tab
+  }
+
 </script>
